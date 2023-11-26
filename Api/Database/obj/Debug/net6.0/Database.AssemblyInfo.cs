76ac012a221f99c4d1a395ee2c5baa61865c6f53
GGeasy75 <rainierypenajrg@gmail.com>
2023-11-26 16:59:02 -0400

    Repository pattern created

--- Api/Database/obj/Debug/net6.0/Database.AssemblyInfo.cs
+++ Api/Database/obj/Debug/net6.0/Database.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+304f273098d661bdd97dfa732af17cfd932793ef")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
