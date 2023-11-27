c03c9fb57e959626c2ac668bb310ec5eb1c8d829
GGeasy75 <rainierypenajrg@gmail.com>
2023-11-26 20:12:40 -0400

    Add identity context y primera migracion creada

--- Api/Database/obj/Debug/net7.0/Database.AssemblyInfo.cs
+++ Api/Database/obj/Debug/net7.0/Database.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Database")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a604a3c640ff6cf0f95c0181dda764502d31d3d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("Database")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Database")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
