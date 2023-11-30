e1e01fd7ca051d1544ba55e37770d170592d5c6d
RainieryPenajrg <rainierypenajrg@gmail.com>
2023-11-30 18:52:20 -0400

    esl comment delete

--- Frontend/src/pages/login/Login.jsx
+++ Frontend/src/pages/login/Login.jsx
@@ -4,6 +4,7 @@ import { useEffect } from "react"
 import LoginForm from "../../components/login/LoginForm"
 
 export default function login({ setShowSidebar }) {
+	// eslint-disable-next-line react-hooks/rules-of-hooks
 	useEffect(() => {
 		setShowSidebar(false)
 	}, [setShowSidebar])
--- Frontend/src/pages/login/profile.jsx
+++ Frontend/src/pages/login/profile.jsx
@@ -30,6 +30,7 @@ export default function Profile() {
 	}
 	useEffect(() => {
 		fetchData()
+		// eslint-disable-next-line react-hooks/exhaustive-deps
 	}, [])
 
 	return (
