55804ff136e2a58bddbae7c3993294f35bb0c8ec
Carlos Arias <101721048+CarlosWAriasA@users.noreply.github.com>
2023-12-02 23:13:59 -0400

    Arreglando que get de animales retorne imagenes

--- Frontend/src/components/AnimalList/AnimalList.jsx
+++ Frontend/src/components/AnimalList/AnimalList.jsx
@@ -15,7 +15,6 @@ const AnimalList = () => {
         }
 
         const animalData = await response.json();
-
         setAnimals(animalData);
         setHasAnimals(animalData.length > 0);
       } catch (error) {
