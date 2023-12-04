3cbb6041c306ba4281ce97fbf1591da805cbb478
Amin <contact@baezamin.com>
2023-12-04 12:54:31 -0400

    Listado de Animales Done

--- Frontend/src/components/AnimalList/AnimalList.jsx
+++ Frontend/src/components/AnimalList/AnimalList.jsx
@@ -1,5 +1,6 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { BASE_URL } from "../../utils/constant";
+import defaultImage from '../../../public/default.jpg'; // Ruta de la imagen por defecto
 
 const AnimalList = () => {
   const [animals, setAnimals] = useState([]);
@@ -27,7 +28,7 @@ const AnimalList = () => {
   }, []);
 
   return (
-    <div>
+    <div style={{ maxHeight: "calc(90vh - 0px)", overflowY: "auto" }}>
       {hasAnimals ? (
         <div
           style={{
@@ -40,35 +41,51 @@ const AnimalList = () => {
             <div
               key={animal.id}
               style={{
-                margin: "10px",
+                margin: "20px",
                 textAlign: "center",
+                position: "relative",
                 boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
                 borderRadius: "8px",
                 overflow: "hidden",
-                maxWidth: "200px",
+                width: "250px",
+                height: "250px",
+                background: "#2d3748",
+                color: "#fff",
+                border: "2px solid #fff",
               }}
             >
               <img
-                src={
-                  animal.imagenes?.length > 0
-                    ? `data:image/jpeg;base64,${animal.imagenes[0]?.content}`
-                    : ""
-                }
+                src={animal.imagenes?.length > 0
+                  ? `data:image/jpeg;base64,${animal.imagenes[0]?.content}`
+                  : defaultImage}
                 alt={animal.nombre}
                 style={{
                   width: "100%",
-                  height: "150px",
+                  height: "200px",
                   objectFit: "cover",
                   borderTopLeftRadius: "8px",
                   borderTopRightRadius: "8px",
                 }}
               />
-              <div style={{ padding: "10px" }}>
+              <div style={{ padding: "10px 10px 5px 10px" }}>
                 <p style={{ fontWeight: "bold", marginBottom: "5px" }}>
                   {animal.nombre}
                 </p>
-                {/* Puedes agregar más información del animal aquí si es necesario */}
               </div>
+              <p
+                style={{
+                  position: "absolute",
+                  top: "5px",
+                  right: "5px",
+                  color: "#fff",
+                  background: "#2d3748",
+                  padding: "3px",
+                  borderRadius: "2px",
+                  fontWeight: "bold",
+                }}
+              >
+                {animal.tipo}
+              </p>
             </div>
           ))}
         </div>
