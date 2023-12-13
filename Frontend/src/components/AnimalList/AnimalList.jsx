af5290db596c753f9feb2b080257b3e24c2c75fc
Carlos Arias <101721048+CarlosWAriasA@users.noreply.github.com>
2023-12-12 21:58:13 -0400

    Quitando console logs

--- Frontend/src/components/AnimalList/AnimalList.jsx
+++ Frontend/src/components/AnimalList/AnimalList.jsx
@@ -5,144 +5,140 @@ import ToastHelper from "../../utils/toast.helper";
 import AnimalSkeleton from "../Skeletons/AnimalSkeleton";
 import { useLocalStorage } from "react-use";
 import { Link } from "react-router-dom";
+import { Tooltip } from "flowbite-react";
 
 const AnimalList = ({ where, fetchData }) => {
-	const [animals, setAnimals] = useState([])
-	const [hasAnimals, setHasAnimals] = useState(false)
-	const [isLoading, setIsLoading] = useState(false)
-	const [user, setUser] = useLocalStorage("user")
+  const [animals, setAnimals] = useState([]);
+  const [hasAnimals, setHasAnimals] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const [user, setUser] = useLocalStorage("user");
 
-	const searchData = async (where) => {
-		setIsLoading(true)
-		try {
-			const queryParams = new URLSearchParams({
-				...where,
-				UsuarioId: user?.userId,
-			}).toString()
-			const url = `${BASE_URL}/Animales?${queryParams}`
+  const searchData = async (where) => {
+    setIsLoading(true);
+    try {
+      const queryParams = new URLSearchParams({
+        ...where,
+        UsuarioId: user?.userId,
+      }).toString();
+      const url = `${BASE_URL}/Animales?${queryParams}`;
 
-			const response = await fetch(url)
+      const response = await fetch(url);
 
-			if (!response.ok) {
-				throw new Error("Error al obtener la lista de animales")
-			}
+      if (!response.ok) {
+        throw new Error("Error al obtener la lista de animales");
+      }
 
-			const animalData = await response.json()
+      const animalData = await response.json();
 
-			setAnimals(animalData)
-      setHasAnimals(animalData.length > 0)
-      console.log(animalData);
-		} catch (error) {
-			console.error("Error al obtener la lista de animales", error)
-			ToastHelper.errorToast(error.message)
-		} finally {
-			setTimeout(() => {
-				setIsLoading(false)
-			}, 1000)
-		}
-	}
+      setAnimals(animalData);
+      setHasAnimals(animalData.length > 0);
+    } catch (error) {
+      console.error("Error al obtener la lista de animales", error);
+      ToastHelper.errorToast(error.message);
+    } finally {
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 1000);
+    }
+  };
 
-	useEffect(() => {
-		searchData()
-	}, [])
+  useEffect(() => {
+    searchData();
+  }, []);
 
-	useEffect(() => {
-		searchData(where)
-	}, [fetchData])
+  useEffect(() => {
+    searchData(where);
+  }, [fetchData]);
 
-	return (
+  return (
+    <div style={{ maxHeight: "calc(90vh - 0px)", overflowY: "auto" }}>
+      {isLoading ? (
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          {Array.from({ length: 8 }, (_, index) => (
+            <AnimalSkeleton key={index} />
+          ))}
+        </div>
+      ) : hasAnimals ? (
+        <div
+          style={{
+            display: "flex",
+            flexWrap: "wrap",
+            justifyContent: "center",
+          }}
+        >
+          {animals.map((animal) => (
+            <div
+              key={animal.id}
+              style={{
+                margin: "20px",
+                textAlign: "center",
+                position: "relative",
+                boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
+                borderRadius: "8px",
+                overflow: "hidden",
+                width: "250px",
+                height: "250px",
+                background: "#2d3748",
+                color: "#fff",
+                border: "2px solid #fff",
+              }}
+            >
+              <Link to={`/animales-detail/${animal.id}`}>
+                <img
+                  src={
+                    animal.imagenes?.length > 0
+                      ? `data:image/jpeg;base64,${animal.imagenes[0]?.content}`
+                      : defaultImage
+                  }
+                  alt={animal.nombre}
+                  style={{
+                    width: "100%",
+                    height: "200px",
+                    objectFit: "cover",
+                    borderTopLeftRadius: "8px",
+                    borderTopRightRadius: "8px",
+                  }}
+                />
+                <div
+                  className="text-white"
+                  style={{ padding: "10px 10px 5px 10px" }}
+                >
+                  <p style={{ fontWeight: "bold", marginBottom: "5px" }}>
+                    {animal.nombre}
+                  </p>
+                </div>
+                <p
+                  className="rounded-lg"
+                  style={{
+                    position: "absolute",
+                    top: "5px",
+                    right: "5px",
+                    color: "#fff",
+                    background: "#2d3748",
+                    padding: "3px",
+                    borderRadius: "2px",
+                    fontWeight: "bold",
+                  }}
+                >
+                  {animal.tipo}
+                </p>
+              </Link>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <p className="mt-44 text-lg" style={{ textAlign: "center" }}>
+          No se encontraron Animales
+        </p>
+      )}
+    </div>
+  );
+};
 
-
-
-		<div style={{ maxHeight: "calc(90vh - 0px)", overflowY: "auto" }}>
-			{isLoading ? (
-				<div
-					style={{
-						display: "flex",
-						flexWrap: "wrap",
-						justifyContent: "center",
-					}}
-				>
-					{Array.from({ length: 8 }, (_, index) => (
-						<AnimalSkeleton key={index} />
-					))}
-				</div>
-			) : hasAnimals ? (
-				<div
-					style={{
-						display: "flex",
-						flexWrap: "wrap",
-						justifyContent: "center",
-					}}
-				>
-					{animals.map((animal) => (
-						<div
-							key={animal.id}
-							style={{
-								margin: "20px",
-								textAlign: "center",
-								position: "relative",
-								boxShadow: "0 4px 8px rgba(0, 0, 0, 0.1)",
-								borderRadius: "8px",
-								overflow: "hidden",
-								width: "250px",
-								height: "250px",
-								background: "#2d3748",
-								color: "#fff",
-								border: "2px solid #fff",
-							}}
-						>
-							<Link to={`/animales-detail/${animal.id}`}>
-							<img
-								src={
-									animal.imagenes?.length > 0
-										? `data:image/jpeg;base64,${animal.imagenes[0]?.content}`
-										: defaultImage
-								}
-								alt={animal.nombre}
-								style={{
-									width: "100%",
-									height: "200px",
-									objectFit: "cover",
-									borderTopLeftRadius: "8px",
-									borderTopRightRadius: "8px",
-								}}
-								/>
-								</Link>
-							<div style={{ padding: "10px 10px 5px 10px" }}>
-								<p style={{ fontWeight: "bold", marginBottom: "5px" }}>
-									{animal.nombre}
-								</p>
-							</div>
-							<p
-								className='rounded-lg'
-								style={{
-									position: "absolute",
-									top: "5px",
-									right: "5px",
-									color: "#fff",
-									background: "#2d3748",
-									padding: "3px",
-									borderRadius: "2px",
-									fontWeight: "bold",
-								}}
-							>
-								{animal.tipo}
-							</p>
-							
-						</div>
-					))}
-				</div>
-			) : (
-				<p className='mt-44 text-lg' style={{ textAlign: "center" }}>
-					No se encontraron Animales
-				</p>
-			)}
-		</div>
-
-
-
-	)
-}
-
-export default AnimalList
+export default AnimalList;
--- Frontend/src/components/login/LogoutButtom.jsx
+++ Frontend/src/components/login/LogoutButtom.jsx
@@ -1,18 +1,17 @@
 // eslint-disable-next-line no-unused-vars
-import React, { useContext } from "react"
-import { AuthContext } from "../../utils/AuthContext"
-import { useNavigate } from "react-router-dom"
+import React, { useContext } from "react";
+import { AuthContext } from "../../utils/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function LogoutButton() {
-	const { logout } = useContext(AuthContext)
-	const navigate = useNavigate()
+  const { logout } = useContext(AuthContext);
+  const navigate = useNavigate();
 
-	const handleLogout = () => {
-		logout()
-		window.localStorage.removeItem("user")
-		navigate("/login")
-		console.log(logout)
-	}
+  const handleLogout = () => {
+    logout();
+    window.localStorage.removeItem("user");
+    navigate("/login");
+  };
 
-	return <button onClick={handleLogout}>Cerrar sesión</button>
+  return <button onClick={handleLogout}>Cerrar sesión</button>;
 }
